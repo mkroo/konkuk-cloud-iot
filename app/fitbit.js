b3b371558cb88311520a25aa27cdda48fd16ef16
ansrl0107 <ansrl0107@gmail.com>
2024-05-07 00:28:34 +0900

    Refactor fetchHealthMetric function to use GET request with query parameters

--- app/fitbit.js
+++ app/fitbit.js
@@ -3,15 +3,13 @@ const { default: axios } = require('axios')
 const dataServerAxios = axios.create({ baseURL: process.env.DATA_SERVER_URL })
 
 const fetchHealthMetric = async (credential) => {
-  // const yesterday = new Date(Date.now() - 86400000).toISOString().slice(0, 10)
-  // const userId = credential.userId
-  // const accessToken = credential.accessToken
+  const yesterday = new Date(Date.now() - 86400000).toISOString().slice(0, 10)
+  const userId = credential.userId
+  const accessToken = credential.accessToken
 
-  // const response = await dataServerAxios.post('/friends', { accessToken, userId, date: yesterday })
+  const response = await dataServerAxios.get('/', { params: { accessToken, userId, date: yesterday } })
 
-  // return JSON.parse(response.data)
-
-  return { userId: 1, date: '2024-10-15', metrics: {} }
+  return JSON.parse(response.data)
 }
 
 module.exports = { fetchHealthMetric }
